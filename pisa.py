3bd1ca3d92f6cf8b9a04e6d5c539fe2a86ccdd02
Chris Glass <christopher.glass@divio.ch>
2011-05-20 11:19:13 +0200

    Fixed the top-level pisa.py file

--- pisa.py
+++ pisa.py
@@ -20,5 +20,5 @@ __author__  = "$Author: holtwick $"
 __date__    = "$Date: 2008-05-31 18:56:27 +0200 (Sa, 31 Mai 2008) $"
 __svnid__   = "$Id: pisa.py 221 2008-05-31 16:56:27Z holtwick $"
 
-import ho.pisa as pisa
+import xhtml2pdf.pisa as pisa
 pisa.command()
