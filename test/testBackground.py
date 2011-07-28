959a2357755fbb3f2ba6fb79ab61bf0329ee480a
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2011-07-28 12:00:27 +0200

    added acceptance test

---
+++ test/testBackground.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#############################################
+## (C)opyright by Dirk Holtwick            ##
+## All rights reserved                     ##
+#############################################
+
+__version__ = "$Revision: 176 $"
+__author__ = "$Author: kgrodzicki $"
+__date__ = "$Date: 2011-01-15 10:11:47 +0100 (Fr, 15 July 2011) $"
+
+"""
+HTML/CSS to PDF converter
+TODO add description
+"""
+
+from cookbook import HTML2PDF
+
+if __name__ == "__main__":
+    xhtml = open('test-background-img.html')
+    HTML2PDF(xhtml.read(), "testBackground.pdf")
