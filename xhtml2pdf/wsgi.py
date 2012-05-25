caa8c9515fe2e3eac22f1fb6b2ea1158dfc7e752
abravetti <a.bravetti@teamsystem.com>
2012-05-25 11:14:43 +0300

    Update xhtml2pdf/wsgi.py

--- xhtml2pdf/wsgi.py
+++ xhtml2pdf/wsgi.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import xhtml2pdf.pisa as pisa
+import pisa
 import StringIO
 
 import logging
