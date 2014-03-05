dc5bbf67efb1a0be7e066034da2dd0b52950b58f
Lukasz Baczynski <baka@MBP-BAka.local>
2014-03-05 15:35:24 +0000

    Ability to set offset on pagenumber

--- xhtml2pdf/tags.py
+++ xhtml2pdf/tags.py
@@ -518,11 +518,12 @@ class pisaTagPDFSPACER(pisaTag):
 
 class pisaTagPDFPAGENUMBER(pisaTag):
     """
-    <pdf:pagenumber example="" />
+    <pdf:pagenumber example="" offset="" />
     """
 
     def start(self, c):
         c.frag.pageNumber = True
+        c.frag.pageOffset = self.attr.offset
         c.addFrag(self.attr.example)
         c.frag.pageNumber = False
 
