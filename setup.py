7b978bd526e833815e0ddf0f9817011b19fb7c42
D. Yu <hello@darylyu.com>
2014-04-27 23:59:43 +0800

    Update package version to 0.0.6 for pypi release.
    Also putting my name in there as the maintainer so everyone can leave
    Chris alone.

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
     name="xhtml2pdf",
-    version="0.0.5",
+    version="0.0.6",
     description="PDF generator using HTML and CSS",
     license="Apache License 2.0",
     author="Dirk Holtwick",
-    maintainer="Christopher Glass",
-    maintainer_email="tribaal@gmail.com",
+    maintainer="Daryl Yu",
+    maintainer_email="hello+pleaseleavemealone@darylyu.com",
     url="http://www.xhtml2pdf.com",
     keywords="PDF, HTML, XHTML, XML, CSS",
     install_requires = ["html5lib", "pyPdf2", "Pillow", "reportlab>=2.2"],
