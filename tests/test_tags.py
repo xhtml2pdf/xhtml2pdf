ed08c721a8ee686e0964388ec1f6bf62704cac16
taddeimania <jtaddei@gmail.com>
2016-05-09 21:17:02 -0400

    Switched pisaTag test case to use AttrContainer

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -2,6 +2,7 @@ import unittest
 from xml.dom import minidom
 
 from xhtml2pdf import tags
+from xhtml2pdf.parser import AttrContainer
 
 
 class PisaTagTestCase(unittest.TestCase):
@@ -9,7 +10,8 @@ class PisaTagTestCase(unittest.TestCase):
     def test_pisa_tag_will_set_attrs_on_init(self):
         dom = minidom.parseString("<unit>test</unit>")
         element = dom.getElementsByTagName("unit")[0]
-        instance = tags.pisaTag(element, "attr")
+        attrs = AttrContainer({})
+        instance = tags.pisaTag(element, attrs)
         self.assertEqual(instance.node, element)
         self.assertEqual(instance.tag, "unit")
-        self.assertEqual(instance.attr, "attr")
+        self.assertEqual(instance.attr, {})
