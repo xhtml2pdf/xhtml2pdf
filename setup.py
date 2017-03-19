067033464d6c2fc189de988386622124234f8022
cclauss <cclauss@bluewin.ch>
2017-03-20 00:12:46 +0100

    Programming Language :: Python :: 3.6

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Documentation',
         'Topic :: Multimedia',
         'Topic :: Office/Business',
