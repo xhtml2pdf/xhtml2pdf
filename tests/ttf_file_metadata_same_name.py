e5438888fd5d881fa24a93740a8fa12e8cf8205a
fbernhart <florian-bernhart@hotmail.com>
2020-09-26 12:03:31 +0200

    Shaddowing

--- tests/ttf_file_metadata_same_name.py
+++ tests/ttf_file_metadata_same_name.py
@@ -8,18 +8,18 @@ output_dir = ('../tests/samples')
 def render_pdf(filename, output_dir):
     basename = os.path.basename(filename)
     outname = '%s.pdf' % os.path.splitext(basename)[0]
-    outfile = os.path.join(output_dir, outname)
-    input = open(filename, 'rb')
-    output = open(outfile, 'wb')
+    out_path = os.path.join(output_dir, outname)
+    input_file = open(filename, 'rb')
+    output_file = open(out_path, 'wb')
 
-    result = pisa.pisaDocument(input, output, path=filename)
+    result = pisa.pisaDocument(input_file, output_file, path=filename)
 
-    input.close()
-    output.close()
+    input_file.close()
+    output_file.close()
 
     if result.err:
         print('Error rendering %s: %s' % (filename, result.err))
         sys.exit(1)
-    return outfile
+    return out_path
 
 render_pdf(filename,output_dir)
