a5fcc2884c5c858dda5d422d1f11f28b6b6c0533
pedros.zg <69447117+pedroszg@users.noreply.github.com>
2020-09-29 17:37:44 -0600

    .

--- tests/test_arabic_font_support.py
+++ tests/test_arabic_font_support.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/python
+# coding=utf-8
 from io import BytesIO
 
 import html5lib
--- tests/test_asian_font_support.py
+++ tests/test_asian_font_support.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/python
+# coding=utf-8
 from io import BytesIO
 from copy import copy
 import unittest
