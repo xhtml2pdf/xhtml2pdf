dadff0d86d66cf0287ab6cc6cdbae02e3809c009
pedros.zg <69447117+pedroszg@users.noreply.github.com>
2020-09-30 13:47:16 -0600

    slide

--- tests/test_update_face_name.py
+++ tests/test_update_face_name.py
@@ -1,10 +1,16 @@
 #-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import io
 from io import BytesIO
 import html5lib
 import unittest
+
+from PyPDF2 import PdfFileReader
+
 from xhtml2pdf.document import pisaDocument
 from xhtml2pdf.w3c.cssDOMElementInterface import CSSDOMElementInterface
 import os
+import tempfile
 
 
 
@@ -18,42 +24,83 @@ __doc__ = """
 
 class ttf_with_same_face_name(unittest.TestCase):
 
+    tests_folder = os.path.dirname(os.path.realpath(__file__))
+    ttf_pathR = os.path.join(tests_folder, 'samples', 'font', 'Microsoft YaHei.ttf')
+    ttf_pathB = os.path.join(tests_folder, 'samples', 'font', 'Microsoft YaHei.ttf')
+    ffR = "@font-face {{font-family: '#MY';src: url(\'{ttf}\');}}".format(ttf=ttf_pathR)
+    ffB = "@font-face {{font-family: MYB;src: url(\'{ttf}\');}}".format(ttf=ttf_pathB)
+    regular = ".regular{font-family:'#MY';}"
+    bold = ".mybold{font-family:MYB;}"
+
+    STRING =    u"""
+                <html>
+                <title></title>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                <style type="text/css">
+                
+                {ffr}
+                
+                {ffb}
+                
+                {myB}
+                
+                {my}
+                
+                </style>
+</head>
+
+<body>
+
+<span class="regular">Regular font type</span>
+<span class="mybold">Bold font type</span>
+
+</body>
+</html>
+    """
+
+    ss = STRING.format(ffr=ffR,ffb=ffB,my=regular,myB=bold)
+
+    print(ss)
+
+
+
     HTML_CONTENT = u"""
-    
-    <html>
-    <head>
-    <title></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <style type="text/css">
+ 
+<html>
+<head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style type="text/css">
 
     @font-face {
         font-family: MYB;
-        src: url('../samples/font/Microsoft YaHei Bold.ttf')
+        src: url('../Microsoft YaHei Bold.ttf')
     }
-
-    @font-face {
-        font-family: '#MY';
-        src: url('../samples/font/Microsoft YaHei.ttf')
-    }
-
+    
+    {{}}
+    
+    
     .regular{
     font-family: '#MY';
-    }   
+}
 
     .mybold{
     font-family: MYB;
-    }
+}
 
-    </style>
-    </head>
 
-    <body>
-    <span class="regular">Regular font type</span>
-    <p class="mybold">Bold font type</p>
+</style>
+</head>
 
-    </body>
-    </html>
+<body>
+
+<span class="regular">Regular font type</span>
+<span class="mybold">Bold font type</span>
+
+</body>
+</html>
     """
+
 
     def test_check_updated_face_name(self):
 
@@ -62,22 +109,23 @@ class ttf_with_same_face_name(unittest.TestCase):
         the font-family from html element are same.
         """
 
-        html = self.HTML_CONTENT
+        html = self.ss
 
         result = BytesIO()
-        pdf = pisaDocument(BytesIO(html.encode('utf-8')), result)
 
+
+        pdf = pisaDocument(BytesIO(self.ss.encode('utf-8')),result)
         parser = html5lib.HTMLParser(tree=html5lib.treebuilders.getTreeBuilder("dom"))
-        document = parser.parse(html)
+        document = parser.parse(self.ss)
         spanElement= document.getElementsByTagName("span")[0]
         spanElement = CSSDOMElementInterface(spanElement)
         attr_name = "font-family"
         rules = pdf.cssCascade.findCSSRulesFor(spanElement, attr_name)
-
         font_family = rules[0][1].get('font-family').strip('#')
         font_pdf = pdf.fontList.get('my')
         if isinstance(font_pdf, str):
             font_pdf = font_pdf.upper()
+
         self.assertEqual(font_family,font_pdf)
 
 
