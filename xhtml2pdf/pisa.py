52d866ef8486dd1897bdd7423e1061bc25267679
fbernhart <florian-bernhart@hotmail.com>
2020-10-03 01:19:05 +0200

    .

--- xhtml2pdf/pisa.py
+++ xhtml2pdf/pisa.py
@@ -102,7 +102,20 @@ See http.client.HTTPSConnection documentation for this parameters
   --http_timeout
 """).strip()
 
-COPYRIGHT = __doc__
+COPYRIGHT = """
+Copyright 2010 Dirk Holtwick, holtwick.it
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License."""
 
 LOG_FORMAT = "%(levelname)s [%(name)s] %(message)s"
 LOG_FORMAT_DEBUG = "%(levelname)s [%(name)s] %(pathname)s line %(lineno)d: %(message)s"
