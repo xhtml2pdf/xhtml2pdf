55c2a645905a3e8a845da34c0cac58d979c1fafd
fbernhart <florian-bernhart@hotmail.com>
2020-10-13 02:03:51 +0200

    Fix for Python 2.7

--- xhtml2pdf/w3c/cssSpecial.py
+++ xhtml2pdf/w3c/cssSpecial.py
@@ -243,7 +243,7 @@ def parseSpecialRules(declarations, debug=0):
             part = getNextPart(parts) or oparts
             if part:
 
-                if isinstance(part, six.text_type) and (("." in part) or ("data:" in part)):
+                if isinstance(part, six.string_types) and (("." in part) or ("data:" in part)):
                     dd.append(("background-image", part, last))
                 else:
                     dd.append(("background-color", part, last))
