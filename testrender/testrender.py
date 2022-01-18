30c13f85a43903a9c29406da13a82c12956c51f7
Nemo <me@captnemo.in>
2022-01-18 15:00:14 +0530

    Update rendertest tox config

--- testrender/testrender.py
+++ testrender/testrender.py
@@ -57,7 +57,8 @@ def create_diff_image(srcfile1, srcfile2, output_dir, options):
 
     outname = '%s.diff%s' % os.path.splitext(srcfile1)
     outfile = os.path.join(output_dir, outname)
-    _,result = exec_cmd(options, options.compare_cmd, srcfile1, srcfile2, '-metric', 'ae', '-lowlight-color','white',  '-colorspace', 'RGB', outfile)
+    # -quiet avoids a colorspace warning
+    _,result = exec_cmd(options, options.compare_cmd, srcfile1, srcfile2, '-quiet', '-metric', 'ae', '-lowlight-color','white', '-colorspace', 'RGB', outfile)
     diff_value = int(float(result.strip()))
     if diff_value > 0:
         if not options.quiet:
